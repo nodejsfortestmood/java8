de0f805ad9f37d9dbe81c7dc7990e105974f0f0e
testmood <testmood@163.com>
2018-08-19 12:21:33 +0800

    k

--- TestDemo/src/com/java8/T4.java
+++ TestDemo/src/com/java8/T4.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class T4 {
 	public static void main(String[] args) {
 		List<Integer> list = new ArrayList<>();
+		list.add(39);
 		list.add(3);
 
 		if (9 == 110) {
@@ -13,8 +14,8 @@ public class T4 {
 			if (9 == 100) {
 				System.out.println("9==10");
 			}
-			if (9 == 100) {
-				System.out.println("9==10");
+			if (9 == 800) {
+				System.out.println("9==82");
 			}
 
 			Integer integer = list.stream().map((i) -> i = i * 3).reduce((c, d) -> c = c + d).get();
