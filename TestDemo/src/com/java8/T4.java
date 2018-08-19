e5bb210688fcdbb99db82bc1bcba1e386fed57a9
testmood <testmood@163.com>
2018-08-19 12:20:53 +0800

    l

--- TestDemo/src/com/java8/T4.java
+++ TestDemo/src/com/java8/T4.java
@@ -7,8 +7,9 @@ public class T4 {
 	public static void main(String[] args) {
 		List<Integer> list = new ArrayList<>();
 		list.add(3);
+		list.add(4);
 
-		if (9 == 110) {
+		if (90 == 110) {
 			System.out.println("55==10");
 			if (9 == 100) {
 				System.out.println("9==10");
