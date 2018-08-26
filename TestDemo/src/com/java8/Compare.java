7c9c8f8dd57b77609c88bdac837ea1e50d8b75a7
testmood <testmood@163.com>
2018-08-26 13:53:40 +0800

    2551

--- TestDemo/src/com/java8/Compare.java
+++ TestDemo/src/com/java8/Compare.java
@@ -1,19 +1,17 @@
 package com.java8;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Compare  implements Comparable<Integer>{
 
 	@Override
 	public int compareTo(Integer o) {
 		
-		
-		// TODO Auto-generated method stub
-		
-		System.out.println("ok  master");
-		System.out.println("ok  dev");
-		
-		if(998==6){
-			System.out.println("899==6");
-		}
+		List<Integer> numbers = Arrays.asList(9, 10, 3, 8, 7, 3, 4,99);
+		List<Integer> distinct = numbers.stream().map( i -> i*i*i).distinct().collect(Collectors.toList());
+		System.out.printf("Original List : %s,  Square Without duplicates : %s %n", numbers, distinct);
 		return 0;
 	}
 
